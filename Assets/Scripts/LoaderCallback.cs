39c5ba929afca0d31d125e85f1a7518894d8ba17
Tommy Tran <tblyduc2412@gmail.com>
2023-12-27 17:46:01 +0700

    feat: Main Menu, Loading.

---
+++ Assets/Scripts/LoaderCallback.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class LoaderCallback : MonoBehaviour
+{
+    private bool isFirstUpdate = true;
+    void Update()
+    {
+        if (isFirstUpdate) { isFirstUpdate = false;
+            Loader.LoaderCallback();
+        }
+    }
+}
